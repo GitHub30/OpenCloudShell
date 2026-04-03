2ae36a49f74f19f3cde50c8b662821beb56a5ec1
GitHub30 <funaox@gmail.com>
2026-04-03 10:38:04 +0000

    connect.phpのリダイレクトURLをHTTPSに更新し、index.htmlのエンドポイントもHTTPSに変更。setup.shにnginxの設定を追加し、nginx.confを新規作成。

--- server/setup.sh
+++ server/setup.sh
@@ -3,7 +3,13 @@ apt install -y nginx php php-fpm certbot python3-certbot-nginx
 systemctl stop apache2
 systemctl disable apache2
 
+nginx -t
+systemctl restart nginx
+
 git clone https://github.com/GitHub30/OpenCloudShell
 vi /etc/nginx/sites-available/default
 vi /etc/default/ttyd
 systemctl restart ttyd
+
+certbot --nginx -d opencloudshell.com -d www.opencloudshell.com
+certbot --nginx -d shell.opencloudshell.com
